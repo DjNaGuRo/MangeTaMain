a03ff27ca6c93c298c089e8b6149b12e0007dc90
DjNaGuRo <guyrostan_nana@yahoo.fr>
2025-11-08 20:15:47 +0100

    fix: src module not found fixed. Streamlit webapp running well either with streamlit run command and docker compose up

--- src/streamlit/app/streamlit_app.py
+++ src/streamlit/app/streamlit_app.py
@@ -5,8 +5,10 @@ import pandas as pd
 import numpy as np
 import sys, inspect
 from pathlib import Path
-
 import yaml
+
+sys.path.insert(0, str(Path(__file__).resolve().parents[3]))
+
 from src.ensure_data import ensure_data
 
 
